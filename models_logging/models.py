1afeba7d0237b543c120a2459cf231f00cc91226
jimmypappachan <jimmypappachan@outlook.com>
2022-10-17 13:40:46 +0530

    User Agent and Ip address field added to Change model

--- models_logging/models.py
+++ models_logging/models.py
@@ -22,14 +22,20 @@ class Revision(models.Model):
     class Meta:
         verbose_name_plural = _('Revisions')
         verbose_name = _('Revision')
-        ordering = ('-pk',)
+        ordering = ('-pk', )
 
-    date_created = models.DateTimeField(_("date created"), db_index=True, auto_now_add=True,
-                                        help_text=_("The date and time this revision was created."))
-    comment = models.TextField(_("comment"), blank=True, help_text=_("A text comment on this revision."))
+    date_created = models.DateTimeField(
+        _("date created"),
+        db_index=True,
+        auto_now_add=True,
+        help_text=_("The date and time this revision was created."))
+    comment = models.TextField(_("comment"),
+                               blank=True,
+                               help_text=_("A text comment on this revision."))
 
     def __str__(self):
-        return 'Revision %s of <%s>' % (self.id, self.date_created.strftime('%Y-%m-%d %H:%M:%S.%f'))
+        return 'Revision %s of <%s>' % (
+            self.id, self.date_created.strftime('%Y-%m-%d %H:%M:%S.%f'))
 
     def get_admin_url(self):
         return reverse('admin:models_logging_revision_change', args=[self.id])
@@ -41,38 +47,59 @@ class Revision(models.Model):
 
 @python_2_unicode_compatible
 class Change(models.Model):
+
     class Meta:
-        ordering = ("-pk",)
+        ordering = ("-pk", )
         verbose_name = _('Changes of object')
         verbose_name_plural = _('All changes')
 
-    ACTIONS = (
-        (ADDED, _("Added")),
-        (CHANGED, _("Changed")),
-        (DELETED, _("Deleted"))
-    )
+    ACTIONS = ((ADDED, _("Added")), (CHANGED, _("Changed")), (DELETED,
+                                                              _("Deleted")))
 
-    date_created = models.DateTimeField(_("Date created"), db_index=True, auto_now_add=True,
-                                        help_text=_("The date and time this changes was."))
-    user = models.ForeignKey(LOGGING_USER_MODEL, blank=True, null=True, on_delete=models.SET_NULL,
-                             verbose_name=_("User"), help_text=_("The user who created this changes."))
+    date_created = models.DateTimeField(
+        _("Date created"),
+        db_index=True,
+        auto_now_add=True,
+        help_text=_("The date and time this changes was."))
+    user = models.ForeignKey(LOGGING_USER_MODEL,
+                             blank=True,
+                             null=True,
+                             on_delete=models.SET_NULL,
+                             verbose_name=_("User"),
+                             help_text=_("The user who created this changes."))
     object_id = models.TextField(
-        help_text=_("Primary key of the model under version control.")
-    )
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
-                                     help_text="Content type of the model under version control.")
+        help_text=_("Primary key of the model under version control."))
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        help_text="Content type of the model under version control.")
     object = GenericForeignKey(ct_field="content_type", fk_field="object_id")
     # TODO: db is not used yet
-    db = models.CharField(max_length=191, help_text=_("The database the model under version control is stored in."))
+    db = models.CharField(
+        max_length=191,
+        help_text=_(
+            "The database the model under version control is stored in."))
 
     changed_data = models.JSONField(blank=True, null=True, encoder=get_encoder)
 
-    object_repr = models.TextField(help_text=_("A string representation of the object."))
-    revision = models.ForeignKey(Revision, blank=True, null=True, verbose_name='to revision', on_delete=models.CASCADE)
-    action = models.CharField(_("Action"), choices=ACTIONS, help_text=_('added|changed|deleted'), max_length=7)
+    object_repr = models.TextField(
+        help_text=_("A string representation of the object."))
+    revision = models.ForeignKey(Revision,
+                                 blank=True,
+                                 null=True,
+                                 verbose_name='to revision',
+                                 on_delete=models.CASCADE)
+    action = models.CharField(_("Action"),
+                              choices=ACTIONS,
+                              help_text=_('added|changed|deleted'),
+                              max_length=7)
+    ip_address = models.GenericIPAddressField(null=True)
+    user_agent=models.TextField(null=True)
 
     def __str__(self):
-        return "Changes %s of %s <%s>" % (self.id, self.object_repr, self.date_created.strftime('%Y-%m-%d %H:%M:%S.%f'))
+        return "Changes %s of %s <%s>" % (
+            self.id, self.object_repr,
+            self.date_created.strftime('%Y-%m-%d %H:%M:%S.%f'))
 
     @staticmethod
     def get_changes_by_obj(model, obj_id, related_objects='__all__'):
@@ -88,7 +115,11 @@ class Change(models.Model):
         """
 
         obj = model.objects.get(pk=obj_id)
-        history_objects = [{'content_type_id': ContentType.objects.get_for_model(model).id, 'values': [obj_id]}]
+        history_objects = [{
+            'content_type_id':
+            ContentType.objects.get_for_model(model).id,
+            'values': [obj_id]
+        }]
         if related_objects == '__all__':
             related_objects = model._meta.related_objects
         for rel_model in related_objects:
@@ -98,26 +129,38 @@ class Change(models.Model):
                 except rel_model.related_model.DoesNotExist:
                     continue
             elif isinstance(rel_model, models.ManyToOneRel):
-                values = getattr(obj, rel_model.get_accessor_name()).all().values_list('pk', flat=True)
+                values = getattr(
+                    obj,
+                    rel_model.get_accessor_name()).all().values_list('pk',
+                                                                     flat=True)
             elif isinstance(rel_model, models.ForeignKey):
                 values = [getattr(obj, rel_model.get_attname())]
             elif isinstance(rel_model, models.ManyToManyField):
-                values = getattr(obj, rel_model.get_attname()).all().values_list('pk', flat=True)
+                values = getattr(
+                    obj, rel_model.get_attname()).all().values_list('pk',
+                                                                    flat=True)
             else:
                 continue
 
-            history_objects.append(
-                {'content_type_id': ContentType.objects.get_for_model(rel_model.related_model).id,
-                 'values': values}
-            )
+            history_objects.append({
+                'content_type_id':
+                ContentType.objects.get_for_model(rel_model.related_model).id,
+                'values':
+                values
+            })
         qobj = models.Q()
         for v in history_objects:
-            qobj.add(models.Q(content_type_id=v['content_type_id'], object_id__in=v['values']), models.Q.OR)
+            qobj.add(
+                models.Q(content_type_id=v['content_type_id'],
+                         object_id__in=v['values']), models.Q.OR)
         return Change.objects.filter(qobj).select_related('user')
 
     def revert(self):
         with transaction.atomic():
-            data = {field: values.get('old') for field, values in self.display_changed_data().items()}
+            data = {
+                field: values.get('old')
+                for field, values in self.display_changed_data().items()
+            }
             if self.action == ADDED:
                 self.object.delete()
             elif self.action == CHANGED:
