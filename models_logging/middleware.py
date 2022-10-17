3d0cf1a5e6b0468939c92c5f01efbcdc0daf90ee
jimmypappachan <jimmypappachan@outlook.com>
2022-10-17 13:44:04 +0530

    ip and user_agent extracted from request

--- models_logging/middleware.py
+++ models_logging/middleware.py
@@ -7,11 +7,21 @@ from . import _local
 from .settings import MERGE_CHANGES
 from .utils import create_revision_with_changes
 
+from user_agents import parse
+
+
 
 class LoggingStackMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
         _local.user = request.user
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[-1].strip()
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        _local.ip_address=ip
+        _local.user_agent = parse(request.META['HTTP_USER_AGENT'])
 
     def process_response(self, request, response):
         if MERGE_CHANGES and _local.stack_changes:
