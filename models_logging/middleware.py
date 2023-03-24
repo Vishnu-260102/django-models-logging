84b90864bc629b625e98877ee86f2c3b17f66bed
jimmypappachan <jimmypappachan@outlook.com>
2023-03-24 14:53:16 +0530

    Auth checking in Middleware implemented - requirements updated
    > Requires Knox(library used to generate token[Default Authentication class in my case]) to verify Token given in headers and to retrieve corresponding user
    > Requires Rest_framework to extract Token from Headers:

--- models_logging/middleware.py
+++ models_logging/middleware.py
@@ -8,26 +8,36 @@ from .settings import MERGE_CHANGES
 from .utils import create_revision_with_changes
 
 from user_agents import parse
-
+from knox.auth import TokenAuthentication
+from rest_framework.authentication import get_authorization_header
 
 
 class LoggingStackMiddleware(MiddlewareMixin):
 
     def process_request(self, request):
+        knoxAuth = TokenAuthentication()
+        auth = get_authorization_header(request).split()
+        if bool(auth):
+            try:
+                user, _ = knoxAuth.authenticate_credentials(
+                    auth[1])
+                request.user = user
+            except Exception:
+                pass
         _local.user = request.user
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
             ip = x_forwarded_for.split(',')[-1].strip()
         else:
             ip = request.META.get('REMOTE_ADDR')
-        _local.ip_address=ip
+        _local.ip_address = ip
         _local.user_agent = parse(request.META['HTTP_USER_AGENT'])
 
     def process_response(self, request, response):
         if MERGE_CHANGES and _local.stack_changes:
             self.create_revision(_local)
         return response
-    
+
     def create_revision(self, _local):
         # this method for overriding and call create_revision_with_changes async maybe
 
